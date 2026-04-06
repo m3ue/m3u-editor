778008cebf5f81543120b8c623d63aeed6e33be1
Shaun Parkison <shparkison@gmail.com>
2026-04-06 09:17:49 -0600

    chore: Organize routes

--- routes/web.php
+++ routes/web.php
@@ -286,14 +286,14 @@ Route::match(['get', 'post'], '/player_api.php', [XtreamApiController::class, 'h
 Route::match(['get', 'post'], '/get.php', [XtreamApiController::class, 'handle'])->name('xtream.api.get');
 Route::get('/xmltv.php', [XtreamApiController::class, 'epg'])->name('xtream.api.epg');
 
-// Stream endpoints
-Route::get('/live/{username}/{password}/{streamId}.{format?}', [XtreamStreamController::class, 'handleLive'])
-    ->name('xtream.stream.live.root');
-
 // Dispatcharr-compatible proxy stream endpoint (used by emby-xtream plugin)
 Route::get('/proxy/ts/stream/{uuid}', [DispatcharrController::class, 'proxyStream'])
     ->name('dispatcharr.proxy.stream')
     ->where('uuid', '[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}');
+
+// Xtream API stream endpoints
+Route::get('/live/{username}/{password}/{streamId}.{format?}', [XtreamStreamController::class, 'handleLive'])
+    ->name('xtream.stream.live.root');
 Route::get('/movie/{username}/{password}/{streamId}.{format?}', [XtreamStreamController::class, 'handleVod'])
     ->name('xtream.stream.vod.root');
 Route::get('/series/{username}/{password}/{streamId}.{format?}', [XtreamStreamController::class, 'handleSeries'])
