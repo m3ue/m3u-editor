667ca30b3a71c0aba9a4f2d6451aeb811ec09859
Shaun Parkison <shparkison@gmail.com>
2026-04-07 10:22:55 -0600

    version bump for release! 🎉

--- config/dev.php
+++ config/dev.php
@@ -3,8 +3,8 @@
 return [
     'author' => 'Shaun Parkison',
     'version' => '0.10.35',
-    'dev_version' => '0.11.20-dev',
-    'experimental_version' => '0.11.20-exp',
+    'dev_version' => '0.11.21-dev',
+    'experimental_version' => '0.11.21-exp',
     'repo' => 'm3ue/m3u-editor',
     'docs_url' => 'https://m3ue.sparkison.dev',
     'donate' => 'https://buymeacoffee.com/shparkison',
