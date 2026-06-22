2c0ebbc9574718dcb67dc1e725d1a3bed2410ca1
Shaun Parkison <shparkison@gmail.com>
2026-06-22 11:09:10 -0600

    chore: Don't show scrubber merge channel toggle unless playlist has merge enabled

--- app/Filament/Resources/ChannelScrubbers/ChannelScrubberResource.php
+++ app/Filament/Resources/ChannelScrubbers/ChannelScrubberResource.php
@@ -291,7 +291,8 @@ class ChannelScrubberResource extends Resource implements CopilotResource
                 ->label(__('Playlist'))
                 ->helperText(__('Select the playlist whose channels you want to scrub.'))
                 ->options(Playlist::where('user_id', Auth::id())->get(['name', 'id'])->pluck('name', 'id'))
-                ->searchable(),
+                ->searchable()
+                ->live(),
             Toggle::make('recurring')
                 ->label(__('Recurring'))
                 ->inline(false)
@@ -391,11 +392,21 @@ class ChannelScrubberResource extends Resource implements CopilotResource
                                 ->visible(fn (Get $get): bool => (bool) $get('scan_all') && (bool) $get('enable_live')),
                             Toggle::make('rebuild_failovers_after_scan')
                                 ->label(__('Rebuild failovers after scan'))
+                                ->columnSpanFull()
                                 ->hintIcon(
                                     'heroicon-s-information-circle',
                                     tooltip: 'After a successful scrubber scan, dispatch the playlist native auto-merge job so failovers can be rebuilt using fresh scan results.',
                                 )
                                 ->helperText(__('Runs native auto-merge after this scrubber completes when playlist auto-merge is enabled.'))
+                                ->hidden(function (Get $get): ?string {
+                                    $playlistId = $get('playlist_id');
+                                    if (! $playlistId) {
+                                        return null;
+                                    }
+                                    $playlist = Playlist::find($playlistId);
+
+                                    return $playlist && ! $playlist->auto_merge_channels_enabled;
+                                })
                                 ->default(false),
                         ]),
                 ]),
